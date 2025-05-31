fe20f2768ffad281a2f6f7b449a38d7ae459015f
SteveWill-iam <if-24017@students.ithb.ac.id>
2025-05-31 16:40:02 +0700

    delete main.js

--- assets/main.js
+++
@@ -1,5 +0,0 @@
-// main.js - Home page AJAX logic
-// Placeholder for AJAX to load folders and shared tasks
-document.addEventListener('DOMContentLoaded', function() {
-    // TODO: AJAX to fetch folders and shared tasks
-});
